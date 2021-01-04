555370804e2e99c365ba04b2f0c3665cde1e2c11
cartodave <dbryson@alteryx.com>
2021-01-04 08:50:37 -0700

    lesson-4-submission

---
+++ udacity-db-deployment/docker/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME='udagrambrysondev'
+export POSTGRES_PASSWORD='Ecu2007!'
+export POSTGRES_HOST='udagrambrysondev.chr9noizrzit.us-east-1.rds.amazonaws.com'
+export POSTGRES_DATABASE='udagrambrysondev'
+export AWS_MEDIA_BUCKET='http://dbayx-udacity-website.s3-website-us-east-1.amazonaws.com'
+export AWS_REGION='us-east-1'
+export AWS_PROFILE='default'
+export JWT_SECRET='hello'
+export URL='http://localhost:8100'
