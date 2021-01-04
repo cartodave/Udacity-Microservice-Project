dbac460558532a750235d4715e5b617077ad5f2a
cartodave <dbryson@alteryx.com>
2021-01-04 13:02:01 -0700

    Lesson-4-submission

--- udacity-db-deployment/k8s/set_env.sh
+++ udacity-db-deployment/k8s/set_env.sh
@@ -3,9 +3,9 @@
 export POSTGRES_USERNAME='udagrambrysondev'
 export POSTGRES_PASSWORD='Ecu2007!'
 export POSTGRES_HOST='udagrambrysondev.chr9noizrzit.us-east-1.rds.amazonaws.com'
-export POSTGRES_DATABASE='udagrambrysondev'
+export POSTGRES_DB='udagrambrysondev'
 export AWS_MEDIA_BUCKET='http://dbayx-udacity-website.s3-website-us-east-1.amazonaws.com'
-export AWS_REGION='us-east-2'
+export AWS_REGION='us-east-1'
 export AWS_PROFILE='default'
 export JWT_SECRET='hello'
-export URL='http://localhost:8100'
+export URL=http://localhost:8100
